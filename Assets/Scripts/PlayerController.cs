b10cb084dcb1147c44e51c0af3b2cee46f102967
Leonid Dashko <all-cs.info@i.ua>
2017-10-22 17:05:06 +0300

    Fix direction of movement after releasing touchpad, add sun and textures

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,11 @@ public class PlayerController : MonoBehaviour {
 
 	private float speed = 0;
 	private float minSpeed = 0;
-	private float maxSpeed = 5f;
+	private float maxSpeed = 10f;
 	private float acceleration = 0.25f;
 	private float deceleration = 0.1f;
 	private Rigidbody rbody;
+	private Quaternion lastTouchPadOrientation;
 
 	void Start() {
 		rbody = GetComponent<Rigidbody>();
@@ -18,27 +19,26 @@ public class PlayerController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		Quaternion ori = GvrControllerInput.Orientation;
-
 		// Move only forward
 //		Vector3 targetMoveVector = ori * (Vector3.forward * Time.deltaTime * 1.5f);
 		
 		// Move in opposite direction
 		// Vector3 targetMoveVector = ori * - (Vector3.forward * Time.deltaTime * 1.5f);
-		
+
+		if (GvrControllerInput.AppButton) {
+			DeadScreen.SetActive(false);
+		}
 		
 		// rbody.velocity = movement * speed;
 		if (GvrControllerInput.ClickButton) { // Speed up by clicking on TouchPad
-//			transform.position += targetMoveVector;
+			lastTouchPadOrientation = GvrControllerInput.Orientation;
 			speed = Mathf.Min(speed + acceleration, maxSpeed);
-			//		targetMoveVector.y = 0;
-//			transform.position += targetMoveVector;
 		} else if (speed > minSpeed) { // Slow down (TouchPad was released
 			speed = Mathf.Max(speed - deceleration, minSpeed);
 		}
 		
 		if (speed > 0) {
-			Vector3 targetMoveVector = ori * (Vector3.forward * Time.deltaTime * (1 + speed));
+			Vector3 targetMoveVector = lastTouchPadOrientation * (Vector3.forward * Time.deltaTime * (1 + speed));
 			targetMoveVector.y = 0;
 			transform.position += targetMoveVector;			
 		}
