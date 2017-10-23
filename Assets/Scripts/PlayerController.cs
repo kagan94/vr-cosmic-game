8b3bf294df85cc2e1125b8318da92a5bf628469d
Leonid Dashko <all-cs.info@i.ua>
2017-10-24 01:11:35 +0300

    Refactoring player's movements, add "reset" functionallity (on app button click)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,52 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	public GameObject DeadScreen;
+    public GameObject deadScreen;
+    public GameObject mainCamera;
 
-	private float speed = 0;
-	private float minSpeed = 0;
-	private float maxSpeed = 10f;
-	private float acceleration = 0.25f;
-	private float deceleration = 0.1f;
-	private Rigidbody rbody;
-	private Quaternion lastTouchPadOrientation;
+    private float speed = 0;
+    private float maxSpeed = 150f;
+    private float acceleration = 0.5f;
+    private Rigidbody rb;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
 
-	void Start() {
-		rbody = GetComponent<Rigidbody>();
-	}
 
-	// Update is called once per frame
-	void Update () {
-		// Move only forward
-//		Vector3 targetMoveVector = ori * (Vector3.forward * Time.deltaTime * 1.5f);
-		
-		// Move in opposite direction
-		// Vector3 targetMoveVector = ori * - (Vector3.forward * Time.deltaTime * 1.5f);
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+    }
 
-		if (GvrControllerInput.AppButton) {
-			DeadScreen.SetActive(false);
-		}
-		
-		// rbody.velocity = movement * speed;
-		if (GvrControllerInput.ClickButton) { // Speed up by clicking on TouchPad
-			lastTouchPadOrientation = GvrControllerInput.Orientation;
-			speed = Mathf.Min(speed + acceleration, maxSpeed);
-		} else if (speed > minSpeed) { // Slow down (TouchPad was released
-			speed = Mathf.Max(speed - deceleration, minSpeed);
-		}
-		
-		if (speed > 0) {
-			Vector3 targetMoveVector = lastTouchPadOrientation * (Vector3.forward * Time.deltaTime * (1 + speed));
-			targetMoveVector.y = 0;
-			transform.position += targetMoveVector;			
-		}
-//		rbody.position += targetMoveVector;
-	}
+    // Update is called once per frame
+    void Update() {
+        // Reset the Player
+        if (GvrControllerInput.AppButton) {
+            deadScreen.SetActive(false);
+            transform.position = initialPosition;
+            rb.velocity = Vector3.zero;
+            
+            float rotationResetSpeed = 50f * Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(transform.rotation, initialRotation, rotationResetSpeed);
+        }
+    }
 
-	private void OnCollisionEnter(Collision other) {
-		if (other.gameObject.CompareTag("Asteroid")) {
-			// Destroy(gameObject);
-			DeadScreen.SetActive(true);
-		}
-	}
+    private void FixedUpdate() {
+        // Speed up by clicking on TouchPad
+        if (GvrControllerInput.ClickButton) {
+            speed = Mathf.Min(speed + acceleration, maxSpeed);
+            Vector3 targetPos = GvrControllerInput.Orientation * Vector3.forward;
+            Vector3 direction = targetPos;
+            rb.AddRelativeForce(direction.normalized * speed, ForceMode.Force);
+        } else {
+            speed = 0;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Asteroid")) {
+            deadScreen.SetActive(true);
+        }
+    }
 }
