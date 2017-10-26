742656896a6ca91c4ccf395326731229db57d1b3
Lin Mao <marklinmao@gmail.com>
2017-10-26 07:41:12 +0300

    Adding air injection particle system, replacing model of sun and earth

---
+++ Assets/Scripts/InjectionControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InjectionControl : MonoBehaviour {
+
+	public GameObject injectionVisualizatin;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Quaternion orientation = GvrControllerInput.Orientation;
+		transform.rotation = orientation;
+
+
+		if (GvrControllerInput.ClickButtonDown) {
+			injectionVisualizatin.SetActive (true);
+		}
+		if (GvrControllerInput.ClickButtonUp) {
+			injectionVisualizatin.SetActive (false);
+		}
+	}
+}
