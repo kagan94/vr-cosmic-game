43391261b59873ce51e658e5ef6548a06c4cc581
Lin Mao <marklinmao@gmail.com>
2017-10-26 12:58:48 +0300

    Adding blinking entrance
    Now we have a blinking entrance in the space station.

---
+++ Assets/EntranceControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntranceControl : MonoBehaviour {
+
+	public Light beacon;
+	public float interval;
+	private float timeElapsed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeElapsed += Time.deltaTime;
+		if(timeElapsed > interval) {
+			if(beacon.enabled == true) {
+				beacon.enabled = false;
+			} else {
+				beacon.enabled = true;
+			}
+			timeElapsed = 0f;
+		}
+	}
+}
