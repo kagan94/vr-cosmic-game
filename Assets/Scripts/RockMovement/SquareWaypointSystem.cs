e5e192d424e95657455df6f733bc1853737389fa
Leonid Dashko <all-cs.info@i.ua>
2017-10-27 17:43:14 +0300

    Add rotating rocks to initial planet

--- Assets/Scripts/RockMovement/SquareWaypointSystem.cs
+++ Assets/Scripts/RockMovement/SquareWaypointSystem.cs
@@ -8,33 +8,23 @@ public class SquareWaypointSystem : MonoBehaviour {
     public float stopDistance = 2.2f;
     public float speed = 5.25f;
     public int destPoint = 0;
-    Vector3[] positionsOfWaypoints = new Vector3[4];//= new List<Vector3>();
+//    Vector3[] positionsOfWaypoints = new Vector3[4];//= new List<Vector3>();
     
     // public Vector3[] points;
     private void Start() {
-        List<Vector3> tmpWaypointsPositions = new List<Vector3>();
-        foreach (Transform waypoint in waypoints) {
-            tmpWaypointsPositions.Add(waypoint.position);
-        }
-        // positionsOfWaypoints = tmpWaypointsPositions.ToArray();
-        positionsOfWaypoints[0] = new Vector3(12, 32, -0.7099972f);
-        positionsOfWaypoints[1] = new Vector3(-2.880859f, 31, -18);
-        positionsOfWaypoints[2] = new Vector3(-2.880859f, 41, -18);
-        positionsOfWaypoints[3] = new Vector3(-2.880859f, 51, -20);
+//        List<Vector3> tmpWaypointsPositions = new List<Vector3>();
+//        foreach (Transform waypoint in waypoints) {
+//            tmpWaypointsPositions.Add(waypoint.position);
+//        }
+//        float duration = 8.5f;
+//        Vector3[] positionsOfWaypoints = tmpWaypointsPositions.ToArray();
+//        
+//        transform.DOPath(positionsOfWaypoints, duration, PathType.CatmullRom, PathMode.Full3D)
+//            .SetLoops(4, LoopType.Restart)
+//            .From(true);
     }
 
     void Update() {
-        float duration = 15.5f;
-//        transform.DOPath(positionsOfWaypoints, duration, PathType.CatmullRom, PathMode.Full3D).SetLoops(5);
-         transform.DOMove(positionsOfWaypoints[1], 10.0f);
-
-//        float dist = Vector3.Distance(gameObject.transform.position, waypoints[destPoint].transform.position);
-//
-//        if (dist < stopDistance) {
-//            destPoint = (destPoint + 1) % waypoints.Count;
-//        } else {
-//            Move();
-//        }
     }
 
 //    void Move() {
---
+++ Assets/Scripts/RotateAroundTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundTarget : MonoBehaviour {
+    public GameObject target;
+    public float speed = 215.5f;
+    public bool oppositeDirection;
+    
+    private Vector3 randomDirection;
+    private int direction;
+    
+    // Use this for initialization
+    void Start() {
+        randomDirection = new Vector3(-Random.value, Random.value, Random.value);
+        speed = 35.5f; // TODO: Remove later, after speed adjustment
+        direction = oppositeDirection ? -1 : 1;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(randomDirection); // rotate object itself
+        transform.RotateAround(target.transform.position, Vector3.one, direction * Time.deltaTime * speed);
+    }
+}
