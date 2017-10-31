3a2aa479697290e2ccfc015276fcb92f09709be9
Leonid Dashko <all-cs.info@i.ua>
2017-10-31 16:31:56 +0200

    Add projection of next player position, mesh colliders to elements on main asteroid, free player position on collision

--- Assets/Scripts/Rock/RockSpawner.cs
+++ Assets/Scripts/Rock/RockSpawner.cs
@@ -10,13 +10,10 @@ public class RockSpawner : MonoBehaviour {
     public GameObject rockPrefab;
 
     [Tooltip("Distance from player where new rock will be generated")]
-    public int distanceToGeneratedRock = 2;
+    public int distanceToGeneratedRock = 40;
 
     [Tooltip("Max Distance from player where new rock will be generated")]
-    public int maxDistance = 1;
-
-    [Tooltip("Generate with random position shift")]
-    public bool randomPosShift = true;
+    public int maxDistance = 220;
 
     [Tooltip("Max Angle between initial and current player pos from Left side (if exceeded, new rock won't be generated)")]
     [Range(0, 180)]
@@ -27,15 +24,21 @@ public class RockSpawner : MonoBehaviour {
     public int maxRightAngle = 30;
 
     public int generateEveryNSeconds = 1;
+    public int sphereRadius = 5;
+    public float rockSpeed = 1.25f;
     public bool debug = true;
-    private float rockSpeed = 25;
-
+    
     private float timeElapsed;
     private Vector3 initialCameraPos;
-
+    private GameObject player;
+    private Rigidbody playerRb;
+    
     void Start() {
         initialCameraPos = Camera.main.transform.position;
+        player = GameObject.Find("Player");
+        playerRb = player.GetComponent<Rigidbody>();
         InstantiateRock();
+        // StartCoroutine("InstantiateRock");
     }
 
     // Update is called once per frame
@@ -47,15 +50,18 @@ public class RockSpawner : MonoBehaviour {
         }
     }
 
-    void InstantiateRock() {
-        Vector3 startPos = transform.position;
+    void OnDrawGizmos() {
         if (debug)
-            Debug.DrawLine(startPos, initialCameraPos, Color.green, Mathf.Infinity);
+            Gizmos.DrawSphere(transform.position, sphereRadius);
+    }
+
+    void InstantiateRock() {
+        // Generate rand position in rock spawner sphere
+        Vector3 startPos = (Random.insideUnitSphere * sphereRadius) + transform.position;
+        if (debug) // draw new random start pos
+            Debug.DrawLine(startPos, startPos + Vector3.forward, Color.red, 1);
         
         Vector3 targetPos = Camera.main.transform.position; // pos of player head
-        if (randomPosShift) {
-            targetPos += new Vector3(Random.Range(-1.5f, 1.5f), 0, 0);
-        }
 
         Vector3 initialDirectionVector = initialCameraPos - startPos;
         Vector3 currentDirectionVector = targetPos - startPos;
@@ -66,25 +72,34 @@ public class RockSpawner : MonoBehaviour {
         float distanceToPlayer = Vector3.Distance(targetPos, startPos);
         if (debug)
             Debug.Log("Distance to player from RockSpawner: " + distanceToPlayer);
-
+        
         if (distanceToPlayer >= distanceToGeneratedRock && distanceToPlayer <= maxDistance && AngleNotExceeded(angle)) {
             // Calculate relative distance [0.0; 1.0] (from Player vector) where new Rock will be generated.
             float relativePos = distanceToGeneratedRock / distanceToPlayer;
             if (debug)
                 Debug.Log("Relative distance where new rock will be generated [0;1]: " + relativePos);
-
+            
+            // Change the distance where new rock will be generated 
             Vector3 newRockPos = Vector3.Lerp(targetPos, startPos, relativePos);
-            Vector3 directionLine = targetPos - newRockPos;
-
+            startPos = newRockPos;
+            
+            // Set target pos to Predicted pos
+            Vector3 projectedPos = predictedPosition(targetPos, startPos, playerRb.velocity, rockSpeed);
+            targetPos = projectedPos;
+            
+            // GameObject newRock = Instantiate(rockPrefab, newRockPos, transform.rotation);
             GameObject newRock = Instantiate(rockPrefab, newRockPos, transform.rotation);
-            Rigidbody newRockRigidbody = newRock.GetComponent<Rigidbody>();
-            newRockRigidbody.AddForce(directionLine.normalized * rockSpeed);
-
+            Rigidbody newRockRb = newRock.GetComponent<Rigidbody>();
             newRock.GetComponent<RockSelfDestory>().SetInitialRockPos(newRockPos);
+            
+            Vector3 directionLine = targetPos - newRockPos;
+            Vector3 force = directionLine.normalized * rockSpeed;
+            newRockRb.AddForce(force, ForceMode.VelocityChange);
             
             if (debug)
                 Debug.DrawLine(startPos, targetPos, Color.red, Mathf.Infinity);
         }
+        // yield return new WaitForSeconds(0.0f);
     }
 
     // Detect the angle between 2 directional vectors with sign 
@@ -100,5 +115,20 @@ public class RockSpawner : MonoBehaviour {
         if (angle < 0 && Mathf.Abs(angle) <= maxRightAngle)
             return true;
         return false;
+    }
+    
+    private Vector3 predictedPosition(Vector3 targetPosition, Vector3 shooterPosition, Vector3 targetVelocity, float projectileSpeed) {
+        Vector3 displacement = targetPosition - shooterPosition;
+        float targetMoveAngle = Vector3.Angle(-displacement, targetVelocity) * Mathf.Deg2Rad;
+        //if the target is stopping or if it is impossible for the projectile to catch up with the target (Sine Formula)
+        if (targetVelocity.magnitude == 0 || targetVelocity.magnitude > projectileSpeed
+            && Mathf.Sin(targetMoveAngle) / projectileSpeed > Mathf.Cos(targetMoveAngle) / targetVelocity.magnitude
+        ) {
+            // Debug.Log("Position prediction is not feasible.");
+            return targetPosition;
+        }
+        //also Sine Formula
+        float shootAngle = Mathf.Asin(Mathf.Sin(targetMoveAngle) * targetVelocity.magnitude / projectileSpeed);
+        return targetPosition + targetVelocity * displacement.magnitude / Mathf.Sin(Mathf.PI - targetMoveAngle - shootAngle) * Mathf.Sin(shootAngle) / targetVelocity.magnitude;
     }
 }
