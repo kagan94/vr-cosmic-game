0a6832d7cb023e32337eb73b78147d0830ec4924
Lin Mao <marklinmao@gmail.com>
2017-11-10 10:02:26 +0200

    Added scene for Demo
    What's new:
    - Introduced a simple state machine for 4 states: InitState, PlayingState, FailureState, SuccessState. Some code in PlayerControl(@Leo please take a look) is adapted to the state machine.
    - Canvas: We have quit many canvas now: InitCanvas, GamePlaying canvas, gameOvercanvas and successCanvas.
    - Canvas: We have 2 alarm screens, for oxygen alarm and fire alarm(at the beginning). Take a try!
    - Oxygen control and alerts.
    - Reset: Now reset is perfect(@Leo: I found there is a simple way, we don't need to do it ourselves:-) Now reset can be done by holding App button for 5 seconds and release it. (Yeah you need to long-press it, single pressing the App button can be used for other cool purpose, such as LASER~)
    - Removed the helm so that the view is clearer! Now the astronaut model have NO head! :-)
    - Removed the Dust so that the view on the astroid is clearer and performance is better!
    - Models: Adjusted the distance of earth, moon and venus. Adjusted the position of thing on the astroid.
    - Rocks: Tested a large amount of Static rocks 'forest' with self-rotation works fine, no performance issue. We can add dynamic rocks(shooting) as well. We can make the dynamic one lighter(maybe with smaller model and less number of rocks?) to make sure it's not laggy.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject canvasInit;
+	public GameObject canvasPlaying;
+	public GameObject canvasFailure;
+	public GameObject canvasSuccess;
+	public const String sceneName = "Demo";
+
+	public GameObject player;
+	public GameObject distanceValue;
+	public GameObject timeSpentValue;
+	public GameObject oxygenConsummedValue;
+
+	public enum GameState {InitState, PlayingState, FailureState, SuccessState};
+	public GameState currentState;
+
+	private PlayerController playerController;
+	private float lastStateChange = 0.0f;
+	private float maxLastingTimeAfterFailure = 5.0f;
+	private float successDistance = 5.0f;
+	private float timestampGameStart = 0.0f;
+
+	void Start () {
+		playerController = (PlayerController) player.GetComponent(typeof(PlayerController));
+		SetState (GameState.InitState);
+		timestampGameStart = Time.time;
+	}
+
+	void Update () {
+		switch (currentState) {
+		case GameState.InitState:
+			canvasInit.SetActive (true);
+			canvasPlaying.SetActive (false);
+			canvasFailure.SetActive (false);
+			canvasSuccess.SetActive (false);
+			break;
+		case GameState.PlayingState:
+			canvasInit.SetActive (false);
+			canvasPlaying.SetActive (true);
+			canvasFailure.SetActive (false);
+			canvasSuccess.SetActive (false);
+			if (playerController.GetDistance () < successDistance) {
+				SwitchToSuccessState ();
+			}
+			distanceValue.GetComponent<Text> ().text = Convert.ToString (playerController.GetDistance ());
+			break;
+		case GameState.FailureState:
+			canvasInit.SetActive (false);
+			canvasPlaying.SetActive (false);
+			canvasFailure.SetActive (true);
+			canvasSuccess.SetActive (false);
+			if (GetStateElapsed () > maxLastingTimeAfterFailure) {
+				SwitchToInitState ();
+			}
+			break;
+		case GameState.SuccessState:
+			canvasInit.SetActive (false);
+			canvasPlaying.SetActive (false);
+			canvasFailure.SetActive (false);
+			canvasSuccess.SetActive (true);
+
+			//calculate the time cost
+			float timeCost = Time.time - timestampGameStart;
+			timeSpentValue.GetComponent<Text> ().text = Convert.ToString (timeCost);
+			oxygenConsummedValue.GetComponent<Text> ().text = Convert.ToString (100 - playerController.oxygenVolumn);
+
+			//Pause the world! This is a trick to pause the world without actually pausing everything...
+			Time.timeScale = 0.01f;
+			if (GetStateElapsed () > 0.05f) {
+				Time.timeScale = 1;
+				SwitchToInitState ();
+			}
+			break;
+		}
+
+		Debug.Log ("Current state: " + currentState);
+	}
+		
+	private void SetState(GameState state) {
+		currentState = state;
+		lastStateChange = Time.time;
+	}
+
+	public void SwitchToPlayingState() {
+		SetState(GameState.PlayingState);
+	}
+
+	public void SwitchToFailureState() {
+		SetState(GameState.FailureState);
+	}
+
+	public void SwitchToSuccessState() {
+		SetState(GameState.SuccessState);
+	}
+
+	public void SwitchToInitState() {
+		SceneManager.LoadScene (GameManager.sceneName); //we need to reset the game for init state!
+		timestampGameStart = Time.time;
+	}
+
+	public void Quit() {
+		Application.Quit ();
+	}
+
+	private float GetStateElapsed() {
+		return Time.time - lastStateChange;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerController : MonoBehaviour {
-	public GameObject deadScreen;
+	
+//	public GameObject deadScreen;
+	private GameManager gameManager;
+	public Transform destination;
+	public Transform departure;
+	public GameObject oxygenValue;
+	public GameObject oxygenAlarmScreen;
+	public GameObject fireAlarmScreen;
 
 	private float speed = 0;
 	private float maxSpeed = 150f;
@@ -13,43 +23,102 @@ public class PlayerController : MonoBehaviour {
 	private Quaternion initialRotation;
 	private Quaternion initialCameraRotation;
 
+	public float oxygenVolumn = 100;
+	private float oxygenConsumingSpeedByAirInjection = 2f;
+	private float oxygenConsumingSpeedByBreath = 0.002f;
+
 	void Start() {
 		rb = GetComponent<Rigidbody>();
 		initialPosition = transform.position;
 		initialRotation = transform.rotation;
 		initialCameraRotation = Camera.main.transform.rotation;
+		gameManager = (GameManager) Camera.main.GetComponent(typeof(GameManager));
 	}
 
-	// Update is called once per frame
 	void Update() {
-		// Reset the Player position
-		if (GvrControllerInput.AppButton) {
-			deadScreen.SetActive(false);
-			transform.position = initialPosition;
-			rb.velocity = Vector3.zero;
-            
-			float rotationResetSpeed = 50f * Time.deltaTime;
-			 transform.rotation = Quaternion.Slerp(transform.rotation, initialRotation, rotationResetSpeed);
-			// transform.rotation = Quaternion.Slerp(transform.rotation, initialRotation, rotationResetSpeed);
-			// Camera.main.transform.rotation = Quaternion.Slerp(Camera.main.transform.rotation, initialCameraRotation, rotationResetSpeed);
+		// Reset the scene
+		if (isAppBtnLongPressed(5)) {
+////			deadScreen.SetActive(false);
+//			transform.position = initialPosition;
+//			rb.velocity = Vector3.zero;
+//            
+//			float rotationResetSpeed = 50f * Time.deltaTime;
+//			 transform.rotation = Quaternion.Slerp(transform.rotation, initialRotation, rotationResetSpeed);
+//			// transform.rotation = Quaternion.Slerp(transform.rotation, initialRotation, rotationResetSpeed);
+//			// Camera.main.transform.rotation = Quaternion.Slerp(Camera.main.transform.rotation, initialCameraRotation, rotationResetSpeed);
+			gameManager.SwitchToInitState ();
+		}
+
+		if (oxygenVolumn < 25 && oxygenVolumn > 0) {
+			showAlarmScreen(oxygenAlarmScreen, 1f); //blink every 1 second
+		}
+		if (oxygenVolumn <= 0) {
+			gameManager.SwitchToFailureState ();
+		}
+
+		if (Vector3.Distance (departure.position, transform.position) < 35) {
+			showAlarmScreen(fireAlarmScreen, 1f);
 		}
 	}
 
+	private float timeElapsed = 0f;
+	private void showAlarmScreen(GameObject alarmScreenObject, float interval) {
+		timeElapsed += Time.deltaTime;
+		if(timeElapsed > interval) {
+			if(alarmScreenObject.activeSelf== true) {
+				alarmScreenObject.SetActive (false);
+			} else {
+				alarmScreenObject.SetActive (true);
+			}
+			timeElapsed = 0f;
+		}
+	}
+
+	private float timestampOfAppBtnDown = 0.0f;
+	private bool isAppBtnLongPressed(float lastingInSeconds) {
+		if (GvrControllerInput.AppButtonDown)
+			timestampOfAppBtnDown = Time.time;
+		if (GvrControllerInput.AppButtonUp) {
+			float timePassed = Time.time - timestampOfAppBtnDown;
+			return (timePassed >= lastingInSeconds) ? true : false;
+		}
+		return false;
+	}
+
+	private float timestampOfClickBtnDown = 0.0f;
 	private void FixedUpdate() {
-		// Speed up by clicking on TouchPad
-		if (GvrControllerInput.ClickButton) {
-			speed += acceleration;
-			Vector3 targetPos = GvrControllerInput.Orientation * Vector3.forward;
-			Vector3 direction = targetPos;
-			rb.AddRelativeForce(direction.normalized * speed, ForceMode.Force);
-		} else {
-			speed = 0;
+		if (gameManager.currentState == GameManager.GameState.PlayingState) {
+			
+			// Speed up by clicking on TouchPad
+			if (GvrControllerInput.ClickButton) {
+				speed += acceleration;
+				Vector3 targetPos = GvrControllerInput.Orientation * Vector3.forward;
+				Vector3 direction = targetPos;
+				rb.AddRelativeForce(direction.normalized * speed, ForceMode.Force);
+			} else {
+				speed = 0;
+			}
+
+			// Oxygen consuming calculation and display
+			oxygenVolumn -= oxygenConsumingSpeedByBreath; //for breath
+			if (GvrControllerInput.ClickButtonDown)
+				timestampOfClickBtnDown = Time.time;
+			if (GvrControllerInput.ClickButtonUp) {
+				float timePassed = Time.time - timestampOfClickBtnDown;
+				oxygenVolumn = oxygenVolumn - timePassed * oxygenConsumingSpeedByAirInjection;
+			}
+			oxygenValue.GetComponent<Text> ().text = oxygenVolumn.ToString("F2");
 		}
 	}
 
 	private void OnCollisionEnter(Collision other) {
-		if (other.gameObject.CompareTag("Asteroid")) {
-			deadScreen.SetActive(true);
-		}
+//		if (other.gameObject.CompareTag("Asteroid")) {
+//			deadScreen.SetActive(true);
+//		}
+		gameManager.SwitchToFailureState();
+	}
+		
+	public float GetDistance() {
+		return Vector3.Distance (transform.position, destination.position);
 	}
 }
